815a65317526e714da2b2e64524e7f1106d7165f
John <john@bogged.finance>
2021-12-03 05:30:29 +0000

    Create Initializer.sol

---
+++ Initializer.sol
@@ -0,0 +1,30 @@
+//SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity ^0.8.7;
+
+/**
+ * $$$$$$$\                                                $$\     $$$$$$$$\ $$\                                                   
+ * $$  __$$\                                               $$ |    $$  _____|\__|                                                  
+ * $$ |  $$ | $$$$$$\   $$$$$$\   $$$$$$\   $$$$$$\   $$$$$$$ |    $$ |      $$\ $$$$$$$\   $$$$$$\  $$$$$$$\   $$$$$$$\  $$$$$$\  
+ * $$$$$$$\ |$$  __$$\ $$  __$$\ $$  __$$\ $$  __$$\ $$  __$$ |    $$$$$\    $$ |$$  __$$\  \____$$\ $$  __$$\ $$  _____|$$  __$$\ 
+ * $$  __$$\ $$ /  $$ |$$ /  $$ |$$ /  $$ |$$$$$$$$ |$$ /  $$ |    $$  __|   $$ |$$ |  $$ | $$$$$$$ |$$ |  $$ |$$ /      $$$$$$$$ |
+ * $$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$   ____|$$ |  $$ |    $$ |      $$ |$$ |  $$ |$$  __$$ |$$ |  $$ |$$ |      $$   ____|
+ * $$$$$$$  |\$$$$$$  |\$$$$$$$ |\$$$$$$$ |\$$$$$$$\ \$$$$$$$ |$$\ $$ |      $$ |$$ |  $$ |\$$$$$$$ |$$ |  $$ |\$$$$$$$\ \$$$$$$$\ 
+ * \_______/  \______/  \____$$ | \____$$ | \_______| \_______|\__|\__|      \__|\__|  \__| \_______|\__|  \__| \_______| \_______|
+ *                     $$\   $$ |$$\   $$ |                                                                                        
+ *                     \$$$$$$  |\$$$$$$  |                                                                                        
+ *                      \______/  \______/
+ * 
+ * https://bogged.finance/
+ */
+
+import "./LibCoreStorage.sol";
+
+// Provides cross-compatibility with current V2 router
+contract Initializer {
+    function initialize() external {
+        LibCoreStorage.coreStorage().implementations[0xcfeb176e] = 0x6bBf1fa4a7eE6525F36aE8ffb6ee22DD009351E3;
+        LibCoreStorage.coreStorage().implementations[0xe0f4e5b2] = 0x6bBf1fa4a7eE6525F36aE8ffb6ee22DD009351E3;
+        LibCoreStorage.coreStorage().implementations[0xdc056eff] = 0x6bBf1fa4a7eE6525F36aE8ffb6ee22DD009351E3;
+    }
+}
